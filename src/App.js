336da87b745c7bac3232d5fcf71aea6c7e65dba4
ericsman <ericlalainar@gmail.com>
2020-10-09 09:39:57 +0300

    nouveau

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-
-
-import Header from './Contacts/Header/Header';
-import Footer from './Contacts/Footer/Footer';
-import HomePage from './Pages/HomePages/HomePage';
-import Contact from './Contacts/Contacts/Contact';
-
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Header from './Pages/Contacts/Header/Header';
+import Footer from './Pages/Contacts/Footer/Footer';
+import HomePage from './Pages/HomePages/HomePage';
+
+
+
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-        
         <HomePage />
-        
-        <Route path="/Contacts/Contacts/Contact" exact component={Contact} />
-
-      
         <Footer />
       </Router>
       
