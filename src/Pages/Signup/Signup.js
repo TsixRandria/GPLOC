b711ef123967289511d9e398b08c5beac10400fa
TsixRandria <randriamamonjisoanantenaina@gmail.com>
2020-10-14 11:44:43 +0300

    ok

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Style.css';
+import './Signup.css';
 
  class Signup extends Component {
     render() {
@@ -9,11 +9,11 @@ import './Style.css';
                 <div id="success" class="success1" >
                 <h1>Inscription</h1>  
                     <form id="contactForm" noValidate className="s-form wow zoomInUp" data-wow-delay="0.5s">
-                    <input type="text" placeholder="VOTRE NOM" defaultValue="" name="user-name" id="user-name" />
-                    <input type="text" placeholder="VOTRE PRENOM" defaultValue="" name="user-name" id="user-name" />
-                    <input type="text" placeholder="TELEPHONE" defaultValue="" name="user-name" id="user-name" />
-                    <input type="text" placeholder="VOTRE EMAIL" defaultValue="" name="user-name" id="user-name" />
-                    <input type="text" placeholder="VOTRE MOT DE PASSE" defaultValue="" name="user-name" id="user-name" />
+                    <input type="text" placeholder="NOM" defaultValue="" name="user-name" id="name" />
+                    <input type="text" placeholder="PRENOM" defaultValue="" name="user-lastname" id="name" />
+                    <input type="text" placeholder="TELEPHONE" defaultValue="" name="user-phone" id="name" />
+                    <input type="text" placeholder="EMAIL" defaultValue="" name="user-email" id="name" />
+                    <input type="text" placeholder="MOT DE PASSE" defaultValue="" name="user-password" id="name" />
                     <button type="submit" className="btn m-btn">INSCRIPTION<span className="fa fa-angle-right"></span></button>
                     </form>
                     </div>
