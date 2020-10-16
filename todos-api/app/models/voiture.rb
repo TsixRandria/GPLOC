4055d78277b4dff101162e5f0e90c6c1310b8b23
jean-claude07 <jeclaude78@gmail.com>
2020-10-16 10:59:45 +0300

    gploc admin

--- todos-api/app/models/voiture.rb
+++
@@ -1,7 +0,0 @@
-class Voiture < ApplicationRecord
-    # model association
-    has_many :voiture_description, dependent: :destroy
-
-    # validations
-    validates_presence_of :marque, :model
-end
