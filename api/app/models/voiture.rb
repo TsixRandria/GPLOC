07f64bdfdecb442e189c77d26c88990338622ab5
TsixRandria <randriamamonjisoanantenaina@gmail.com>
2020-10-16 16:27:46 +0300

    tarif

--- api/app/models/voiture.rb
+++ api/app/models/voiture.rb
@@ -1,6 +1,7 @@
 class Voiture < ApplicationRecord
     # model association
     has_many :voiture_description, dependent: :destroy
+    has_one :tarif
 
     # validations
     validates_presence_of :marque, :model
